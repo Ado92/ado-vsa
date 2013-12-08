f2d62b84bb6b0291ac99fd636a22560d82ba9d81
Ado92 <chocky92@gmail.com>
2013-12-08 02:10:54 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+ printf ("Hello, world");
+ puts "Hello, #{ARGV.first}!"
+   //pridal som si nieco nove do master vetvy <- tento riadok =D 
+ // tento riadok prave pridavam na nete :D
+ //malo by sa toto pridat na net :)
+ return 1;
+}
